21dbed1c285ffcaa83e1908b99a1b95ec0f0682e
Starchy29 <ethanball29@gmail.com>
2023-11-08 22:39:58 -0500

    perhaps completed the assignment

--- City L-System/Assets/Scripts/BuildingSpawner.cs
+++ City L-System/Assets/Scripts/BuildingSpawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BuildingSpawner : MonoBehaviour
 {
-    private static Dictionary<Vector3, GameObject> placedBuildings = new Dictionary<Vector3, GameObject>();
+    [SerializeField] private GameObject TopLeft;
+    [SerializeField] private GameObject TopRight;
+    [SerializeField] private GameObject BottomLeft;
+    [SerializeField] private GameObject BottomRight;
+    private static Dictionary<Vector2Int, List<Vector3>> placedSpots = new Dictionary<Vector2Int, List<Vector3>>();
 
     void Start() {
-        if(placedBuildings.ContainsKey(transform.position)) {
-            Destroy(gameObject);
-            Debug.Log("destroyed duplicate");
-            return;
+        // destroy buildings placed on the same spot
+        Vector2Int region = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+        if(placedSpots.ContainsKey(region)) {
+            foreach(Vector3 position in placedSpots[region]) {
+                if(Vector3.Distance(position, transform.position) <= 0.1f) {
+                    Destroy(gameObject);
+                    return;
+                }
+            }        
+        } else {
+            placedSpots[region] = new List<Vector3>();
         }
 
-        placedBuildings[transform.position] = gameObject;
+        // register this building location
+        placedSpots[region].Add(transform.position);
+
+        // randomly choose which corners to eliminate
+        int numRemoved = 0;
+        GameObject[] corners = new GameObject[4] { TopLeft, TopRight, BottomLeft, BottomRight };
+        foreach(GameObject corner in corners) {
+            if(Random.value <= 0.3f - 0.1f * numRemoved) {
+                Destroy(corner);
+                numRemoved++;
+            }
+        }
     }
 }
